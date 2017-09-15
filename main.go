6c77f8a4f2ba45bc2224fcdaf6de389adb212c53
Thomas Fan <thomasjpfan@gmail.com>
2017-09-15 16:28:34 -0400

    API: Defaults are passed into scaler

--- main.go
+++ main.go
@@ -17,13 +17,17 @@ func main() {
 	client, err := client.NewEnvClient()
 	minLabel := "com.df.scaleMin"
 	maxLabel := "com.df.scaleMax"
+	defaultMin := uint64(1)
+	defaultMax := uint64(20)
 
 	if err != nil {
 		logger.Panicln(err)
 	}
 
 	fmt.Println("Starting Docker Scaler")
-	scaler := service.NewScalerService(client, minLabel, maxLabel)
+	scaler := service.NewScalerService(
+		client, minLabel, maxLabel,
+		defaultMin, defaultMax)
 	s := server.NewServer(scaler, logger)
 	s.Run(8080)
 }
