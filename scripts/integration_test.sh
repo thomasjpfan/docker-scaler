25cf68348decbdf5aad97f35e2bb830f43bee548
Thomas Fan <thomasjpfan@gmail.com>
2017-09-15 14:40:52 -0400

    TST: Travis Fix

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -4,7 +4,7 @@ docker run --rm \
 -v "$(pwd)":/go/src/github.com/thomasjpfan/docker-scaler \
 --network test_scaling \
 -v "/var/run/docker.sock:/var/run/docker.sock" \
--e "SCALER_IP=test_scaler" \
+-e "SCALER_IP=scaler" \
 -e "TARGET_SERVICE=test_web" \
 golang:1.9.0-alpine3.6 \
 go test github.com/thomasjpfan/docker-scaler/integration -v
